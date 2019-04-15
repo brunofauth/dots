f7772118c2a5cc927953cb8d1cd9b7d8fc548b1e
Bruno Fauth <149593@upf.br>
2019-04-14 21:32:48 -0300

    ytdl

--- general/.config/fish/config.fish
+++ general/.config/fish/config.fish
@@ -84,12 +84,16 @@ function medmenu;
     echo -n "$prefix/"(find "$prefix" -type f | sed -E "s|^$prefix/(.+)\$|\1|" | fzf -e -i) | xargs -r -d '\n' xdg-open
 end
 
-function qaudio;
+function qaudio
     set tmp $argv[2]
     if test -z $tmp; set tmp 1.5; end
     set thd $argv[3]
     if test -z $thd; set thd 2%; end
     sox "$argv[1]" "fast-$argv[1]" silence 1 0.1 $thd 1 0.1 $thd tempo $tmp : restart
+end
+
+function ytdl
+    youtube-dl "(xclip -selection clipboard -o)"
 end
 
 function ex # usage: ex <file>
