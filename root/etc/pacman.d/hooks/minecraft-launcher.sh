f5c6e302c0dc120e5e3fb412e067383512354d32
Bruno Fauth <149593@upf.br>
2020-08-31 13:30:49 -0300

    hahaha lol

--- root/etc/pacman.d/hooks/minecraft-launcher.sh
+++ root/etc/pacman.d/hooks/minecraft-launcher.sh
@@ -2,11 +2,13 @@
 
 
 LAUNCHER=/usr/bin/minecraft-launcher.sh
-SHSCRIPT='
-#! /bin/sh
+SHSCRIPT='#! /bin/sh
 
 
-TMPDIR="${XDG_CACHE_DIR:-$HOME/.cache}/mc-tmp" /usr/bin/minecraft-launcher --workDir "${XDG_DATA_HOME:-$HOME/.local/share}/minecraft"
+export TMPDIR="${XDG_CACHE_DIR:-$HOME/.cache}/mc-tmp"
+minecraft_dir="${XDG_DATA_HOME:-$HOME/.local/share}/minecraft"
+
+/usr/bin/minecraft-launcher --workDir "$minecraft_dir"
 
 '
 
