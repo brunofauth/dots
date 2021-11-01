3544256c55c983af4c5e79d28b10484f67251829
Bruno Fauth <149593@upf.br>
2021-11-01 14:07:47 -0300

    if weather.sh fails, it will retry every 5 seconds

--- general/.config/polybar/scripts/weather.sh
+++ general/.config/polybar/scripts/weather.sh
@@ -1,6 +1,12 @@
 #! /bin/sh
 
 
-# sed is used to remove \uFE0F
-curl -sS 'wttr.in/?format=1' | sed 's|️||'
+while true; do
+    # sed is used to remove \uFE0F
+    data="$(curl -s 'wttr.in/?format=1')" \
+        && { echo "$data" | sed 's|️||'; } && exit
+
+    sleep 5
+
+done
 
