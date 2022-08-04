ce76fcdec7453b2d2d1525bf68c3d978f4ed8d5e
Bruno <bvfauth@hotmail.com>
2022-08-04 20:08:49 -0300

    updated fish abbrs

--- general/.config/fish/functions/find-py-deps.fish
+++ general/.config/fish/functions/find-py-deps.fish
@@ -1,7 +1,7 @@
 function find-py-deps --description "Print all dependencies in the given python files"
 
     for file in $argv
-        grep -e '^import' -e '^from' $file
+        grep -e '^\s*import' -e '^\s*from' $file
     end | cut -d ' ' -f2 | cut -d '.' -f1 | sort | uniq
 
 end
