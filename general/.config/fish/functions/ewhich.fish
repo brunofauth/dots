f5ff1ce811b51491a18d11b8f6fc09a7d1c44b56
Bruno Fauth <149593@upf.br>
2023-06-22 14:17:15 -0300

    changed ewhich

--- general/.config/fish/functions/ewhich.fish
+++ general/.config/fish/functions/ewhich.fish
@@ -2,19 +2,14 @@ function ewhich \
     --wraps=which \
     --description 'Use $EDITOR on files found by which'
 
-    set files
-    for name in $argv
-        set file (which $name)
-        if test "$EDITOR" = "vim"
-            set -a files $file
-        else
-            $EDITOR $file
-        end
+    set file (which -- $argv[1])
+    if test -z $file; or ! test -f $file
+        return 1
     end
 
-    echo $files
-
-    if test "$EDITOR" = "vim"
-        $EDITOR -p $files
+    if test ! \( -r $file \) -o ! \( -w $file \)
+        sudo -e $file
+    else
+        $EDITOR $file
     end
 end
