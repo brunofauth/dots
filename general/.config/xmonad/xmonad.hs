ee4f5bebb3ad3e81ccec2c8dfb5fb5dd6b30fcc7
Your Name <you@example.com>
2024-07-09 23:46:19 -0300

    updated i3wm, xmonad and sxhkd;
    removed a lot of redundant things from i3 cfg

--- general/.config/xmonad/xmonad.hs
+++ general/.config/xmonad/xmonad.hs
@@ -1,21 +1,38 @@
 -- vim: foldmethod=marker foldlevel=0
 
 import XMonad
-import XMonad.Actions.CycleWS
+import XMonad.Actions.CycleWS (prevWS, nextWS, shiftToPrev, shiftToNext)
+import XMonad.Util.Run
 import XMonad.Util.EZConfig
-import XMonad.Util.SpawnOnce
-import XMonad.Hooks.Modal
-import XMonad.Hooks.EwmhDesktops
+import XMonad.Util.SpawnOnce (spawnOnce)
+import XMonad.Hooks.Modal (Mode, modal, setMode, mode, mkKeysEz)
+import XMonad.Hooks.EwmhDesktops (ewmh, ewmhFullscreen)
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.Accordion
 import XMonad.Layout.Spacing
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Hidden
+import XMonad.Hooks.ManageHelpers (doCenterFloat)
+import qualified XMonad.StackSet as W
+
+import Data.Maybe
+import Control.Monad.State
 
 
-restartXmonad = spawn
-    $  "if type xmonad;"
-    ++ " then xmonad --recompile && xmonad --restart;"
-    ++ " else xmessage xmonad not in \\$PATH;"
-    ++ " fi"
+withBars :: (Window -> X ()) -> X ()
+withBars func = withDisplay $ \d -> withWindowSet $ \mySet -> do
+    windowsAndProps <- forM (W.allWindows mySet) $ \myWindow -> do
+        prop <- getStringProperty d myWindow "_NET_WM_STRUT"
+        return (myWindow, prop)
+    mapM_ func [myWin | (myWin, prop) <- windowsAndProps, isJust prop]
+
+
+restartXmonad = spawn $ concat
+    [ "if type xmonad;"
+    , " then xmonad --recompile && xmonad --restart;"
+    , " else xmessage xmonad not in \\$PATH;"
+    , " fi"
+    ]
 
 main :: IO ()
 main = xmonad . modal [modeGaps] . docks . ewmhFullscreen . ewmh $ cfg
@@ -26,15 +43,19 @@ main = xmonad . modal [modeGaps] . docks . ewmhFullscreen . ewmh $ cfg
         , "M-S-r", "M-r" -- xinerama screen 3
         , "M-S-p", "M-p" -- dmenu and qmrun
         , "M-S-c" -- kill window (sxhkd does that)
+        , "M-h", "M-l" -- now using < and >
         ]
     `additionalKeysP`
-        [ ("M-b", sendMessage ToggleStruts)
-        , ("M-[", prevWS)
-        , ("M-]", nextWS)
-        , ("M-S-[", shiftToPrev *> prevWS)
-        , ("M-S-]", shiftToNext *> nextWS)
+        -- [ ("M-b", sendMessage ToggleStruts)
+        [ ("M-h", prevWS)
+        , ("M-l", nextWS)
+        , ("M-S-h", shiftToPrev *> prevWS)
+        , ("M-S-l", shiftToNext *> nextWS)
         , ("M-g", setMode modeGapsLabel)
         , ("M-r", restartXmonad)
+        , ("M-S-,", sendMessage Shrink)
+        , ("M-S-.", sendMessage Expand)
+        , ("M-y", withFocused hide)
         ]
 
 cfg = def
@@ -43,29 +64,40 @@ cfg = def
     , layoutHook = myLayoutHook
     , startupHook = myStartupHook
     , manageHook = myManageHook
+    , focusedBorderColor = "red"
     }
 
 myLayoutHook = avoidStruts
-    $ let borderSize = Border 0 0 0 0 in
-        spacingRaw False borderSize True borderSize True
+    $ hiddenWindows 
+    $ spacingRaw False borderSize True borderSize True
+    $ smartBorders
     $ tiled ||| Mirror tiled ||| Full ||| Accordion
-        where
-            tiled   = Tall nmaster delta ratio
-            nmaster = 1     -- The default number of windows in the master pane
-            ratio   = 1/2   -- Default proportion of screen occupied by master pane
-            delta   = 3/100 -- Percent of screen to increment by when resizing panes
+    where
+        tiled   = Tall nmaster delta ratio
+        nmaster = 1     -- The default number of windows in the master pane
+        ratio   = 1/2   -- Default proportion of screen occupied by master pane
+        delta   = 3/100 -- Percent of screen to increment by when resizing panes
+        borderSize = Border 0 0 0 0
 
 myStartupHook = do
     spawnOnce "polybar -c ~/.config/polybar/config.ini main"
 
 myManageHook = composeAll
-    [ className =? "Galculator" --> doFloat
-    , className =? "zenity" --> doFloat
-    , className =? "float-term" --> doFloat
-    , className =? "Tor Browser" --> doFloat
-    , className =? "Lxappearance" --> doFloat
-    , className =? "Pavucontrol" --> doFloat
-    , className =? "System-config-printer.py" --> doFloat
+    [ className =? "Galculator" --> doCenterFloat
+    , className =? "zenity" --> doCenterFloat
+    , className =? "float-term" --> doCenterFloat
+    , className =? "Tor Browser" --> doCenterFloat
+    , className =? "Lxappearance" --> doCenterFloat
+    , className =? "Pavucontrol" --> doCenterFloat
+    , className =? "System-config-printer.py" --> doCenterFloat
+    , className =? "Pinentry" --> doCenterFloat
+    , className =? "Xmessage" --> doCenterFloat
+    , className =? "confirm" --> doCenterFloat
+    , className =? "file_progress" --> doCenterFloat
+    , className =? "dialog" --> doCenterFloat
+    , className =? "download" --> doCenterFloat
+    , className =? "error" --> doCenterFloat
+
     , className =? "Zathura" --> doShift "4"
     , className =? "firefox" --> doShift "2"
     ]
