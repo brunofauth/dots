d65d00f29bf37b8a6cfd2b7dd16e1797aec12fca
Your Name <you@example.com>
2024-09-01 01:20:37 -0300

    removed unneeded pacman hooks, refactored system resource pathes; added a new pacman hook

--- root/etc/pacman.d/hooks/pokemmo.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-# this could be optimized to quit after the first substitution but I digress
-sed -i 's|POKEMMO="$HOME/.pokemmo"|POKEMMO="$XDG_DATA_HOME/pokemmo"|' /usr/bin/pokemmo-launcher
-
