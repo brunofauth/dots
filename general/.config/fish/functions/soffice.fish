618d3315e4e7adb22f85be70629056763b79a2cc
Bruno Fauth <bvfauth@hotmail.com>
2025-12-15 16:13:01 -0300

    added cargo config, fixed typos

--- general/.config/fish/functions/soffice.fish
+++ general/.config/fish/functions/soffice.fish
@@ -4,8 +4,8 @@ end
 
 
 function soffice --wraps /usr/bin/soffice
-    if test (count $argv) -eq 1 && test -f $argv 1
-        command soffice $argv 1 &>/dev/null & disown
+    if test (count $argv) -eq 1 && test -f $argv
+        command soffice $argv &>/dev/null & disown
     end
     command soffice $argv
 end
